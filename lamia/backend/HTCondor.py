687b7076792613b1132b3165e61161e395b22405
Clint09 <gecko09@outlook.com>
2018-10-16 23:37:11 +0200

    the first version of htcondor backend with python bindings

--- lamia/backend/HTCondor.py
+++ lamia/backend/HTCondor.py
@@ -19,7 +19,7 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import logging, os, copy, subprocess  #, htcondor
+import logging, os, copy, subprocess, htcondor
 import lamia.core.filesystem
 import lamia.core.templates
 import lamia.backend.interface
@@ -29,12 +29,21 @@ import lamia.backend.interface
 The rotuines for task submission on HTCondor clusters steered by BASH script.
 """
 
-gSubConfigs = {'output'                  : 'output/HTCondor.out',
-               'error'                   : 'error/HTCondor.err',
-               'log'                     : 'log/HTCondor.log',
-               'should_transfer_files'   : 'YES',
-               'when_to_transfer_output' : 'ON_EXIT'
-               }
+gSubConfigs = {'output'                   : 'output/HTCondor.out',
+               'error'                    : 'error/HTCondor.err',
+               'log'                      : 'log/HTCondor.log',
+               'should_transfer_files'    : 'YES',
+               'when_to_transfer_output'  : 'ON_EXIT'
+              }
+
+gPySubConfigs = {'cmd'                    : '',
+                 'arguments'              : '',
+                 'output'                 : 'output/HTCondor.out',
+                 'error'                  : 'error/HTCondor.err',
+                 'userLog'                : 'log/HTCondor.log',
+                 'ShouldTransferFiles'    : 'YES',
+                 'when_to_transfer_output':'ON_EXIT'
+                }
 
 class HTCondorBackend(lamia.backend.interface.BatchBackend):
     """
@@ -65,33 +74,30 @@ TODO: Detailed description
         """
         L = logging.getLogger(__name__)
         
-        L.debug("test of config:")
-        L.debug("condor host = %s", self.cfg['CONDOR_HOST'])
-
         # creating .sub file
         f = open('file.sub', 'w+')
         if (type(cmd) is not list):
             raise TypeError( "First argument is expected to be a list' \
                             ' not a %s"%type(cmd) )
-        L.debug("\n")
-        L.debug("   Started to build file.sub")
+        L.debug("Started to build file.sub")
         f.write("executable = {0}\n".format(cmd[0]))
-        L.debug("executable = %s", cmd[0])
-        if not 'output':
+        f.write("arguments = ")
+        for i in range(len(cmd))[1:]:
+            f.write("{0} ".format(cmd[i]))
+        f.write("\n")
+        if stdout:
             gSubConfigs.update({'output' : stdout}) 
-        if not 'error':
+        if stderr:
             gSubConfigs.update({'error'  : stderr})
         for k, v in gSubConfigs.items():
             f.write("{0} = {1}\n".format(k, v))
-            L.debug("%s = %s"%(k, v))
         f.write("queue 1\n")
-        L.debug("queue 1")
         f.close()
-        L.debug("   file.sub was built\n")
+        L.debug("file.sub was built\n")
         
         #forming the command line
         cmd_ = ['condor_submit', 'file.sub']
-        L.debug("Command words are %s"%cmd_)
+        L.debug("Command for submittion is %s"%(" ".join(cmd_)))
         #submitting the job
         pkw = copy.deepcopy({ 'stdout' : subprocess.PIPE
                             , 'stderr' : subprocess.PIPE
@@ -120,7 +126,7 @@ TODO: Detailed description
 
 
 
-class HTCondorBackendPy(lamia.backend.interface.BatchBackend):
+class HTCondorPyBackend(lamia.backend.interface.BatchBackend):
     """
 TODO: Short description
 
@@ -153,36 +159,19 @@ TODO: Detailed description
         if (type(cmd) is not list):
             raise TypeError( "First argument is expected to be a list' \
                             ' not a %s"%type(cmd) )
-        L.debug("\n")
-        L.debug("   Started to build file.sub")
-        sub = htcondor.Submit()
-        sub['executable'] = cmd[0]
-        L.debug("executable = %s", cmd[0])
-        if not 'output':
-            gSubConfigs.update({'output' : stdout}) 
-        if not 'error':
-            gSubConfigs.update({'error'  : stderr})
-        for k, v in gSubConfigs.items():
-            f.write("{0} = {1}\n".format(k, v))
-            L.debug("%s = %s"%(k, v))
-# TODO: to understand what is queue        
-        f.write("queue 1\n")
-        L.debug("queue 1")
-        f.close()
-        L.debug("   file.sub was built\n")
         
-        #forming the command line
-        cmd_ = ['condor_submit', 'file.sub']
-        L.debug("Command words are %s"%cmd_)
+        if stdout:
+            gPySubConfigs.update({'output' : stdout}) 
+        if stderr:
+            gPySubConfigs.update({'error' : stderr})
+        gPySubConfigs['cmd'] = cmd[0]
+        gPySubConfigs['arguments'] = ' '.join(cmd[1:])
+        
         #submitting the job
-# TODO: to form actual stdout and stderr        
-        pkw = copy.deepcopy({ 'stdout' : subprocess.PIPE
-                            , 'stderr' : subprocess.PIPE
-                            , 'universal_newlines' : True })
-        pkw.update(popenKwargs)
-        L.debug("Popen arguments are %s"%pkw)
-        submJob = subprocess.Popen(cmd_, **pkw)
-        #message = subprocess.check_output(cmd_)
+        schedd = htcondor.Schedd()
+        schedd.submit(gPySubConfigs)
+       
+        L.debug("Command is %s"%(' '.join(cmd)))
 # TODO: to take configs from the config directory, not from local
 # TODO: to build the properties to return
         return {'jID':'1', 'queue':'1'}
